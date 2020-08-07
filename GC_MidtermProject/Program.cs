a6b0570e0d2a93bf309efabac72d3e28529a5467
ramez <herrickramez@gmail.com>
2020-08-07 10:56:31 -0400

    Ramez version

--- GC_MidtermProject/Program.cs
+++ GC_MidtermProject/Program.cs
@@ -6,7 +6,7 @@ namespace GC_MidtermProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ramez");
         }
     }
 }
