ee672508ded893af964615aef733fbc76cfeaf56
justin <justin.d.locke@gmail.com>
2020-08-07 13:39:26 -0400

    v1

--- GC_MidtermProject/Program.cs
+++ GC_MidtermProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace GC_MidtermProject
 {
@@ -6,9 +8,22 @@ namespace GC_MidtermProject
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>() { };
+            products.Add(new Product("Cheese", Category.Food, "Delicious Wisonsin Cheddar", 4.99));
+            products.Add(new Product("Milk", Category.Drink, "2% Milk - Super Fresh!", 3.49));
+            products.Add(new Product("Beef", Category.Food, "1 lb 100% Grass Fed Beef", 6.99));
+            products.Add(new Product("Television", Category.HardGood, "85\" 8k 5G Enabled with 10 free Nic Cage movies", 4999.00));
+            products.Add(new Product("Stuffed Penguin", Category.HardGood, "Stuffed Friend Always Dressed for the Occasion", 14.99));
+            products.Add(new Product("Polka Dot Rain Coat", Category.SoftGood, "Stay dry, in style!", 19.99));
 
-            Console.WriteLine("I've got a bad feeling about this, said Justin");
 
+            StreamWriter writer = new StreamWriter("../../../ProductList.txt");
+
+            foreach (Product product in products)
+            {
+                writer.WriteLine(product);
+            }
+            writer.Close();
 
         }
     }
