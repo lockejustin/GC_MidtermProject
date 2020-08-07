2a5a019c5cff3c1e01be2c4e6e37eff78d589078
JordanHallam <jghals12@gmail.com>
2020-08-07 13:44:00 -0400

    jordan v1

--- GC_MidtermProject/Program.cs
+++ GC_MidtermProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace GC_MidtermProject
 {
@@ -6,9 +8,52 @@ namespace GC_MidtermProject
     {
         static void Main(string[] args)
         {
+           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            List<string> inventory = new List<string>
+            {
+                "stuff" 
+            };
+            List<string> shoppingCart = new List<string>();
 
-            Console.WriteLine("I've got a bad feeling about this, said Justin");
 
+        }
+        public static int ShoppingMenu(List<string> inventory, List<string>shoppingCart)
+        {
+            while (true)
+            {
+
+                for (int index = 0; index < inventory.Count; index++)
+                {
+                    Console.WriteLine($"{(index + 1)} {inventory[index]}");
+                }
+
+                Console.WriteLine("Which item would you like to add to your cart?");
+                int itemselection = int.Parse(Console.ReadLine()) - 1;
+                shoppingCart.Add(inventory[itemselection]);
+
+                Console.WriteLine($"How many {inventory[itemselection]}(s) would you like?");
+                int quantity = int.Parse(Console.ReadLine());
+                return quantity;
+            }
 
         }
     }
